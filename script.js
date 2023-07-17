cafbb564c7ddcedf4edefb26c952060b68d5d576
Emilie <emiliewu@163.com>
2023-07-17 10:47:49 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  // 1. get input value from #username.
+
+  //...your code
+    
+  // 2. write a search function using ajax to get the user's repository list.
+
+  //...your code
+
+  
+  //3. write a function to display the list on DOM
+
+  // ... your code
+
+
+  });
+  
